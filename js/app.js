ccb8197fc4f7e3f8df0e3026d2a193713b982ed1
Tiana Tucker <tianatucker@gmail.com>
2016-09-03 19:50:11 -0400

    add animations

--- js/app.js
+++ js/app.js
@@ -1,12 +1,33 @@
-$(document).ready(function() {
-	//When mouse enters the div Ryu assumes the ready position
-	$('.ryu').mouseenter(function() {
-    $('.ryu-still').hide();
-	$('.ryu-ready').show();
-  })
-	//When mouse leaves the div Ryu is still
-	.mouseleave(function() {
-    $('.ryu-ready').hide();
-	$('.ryu-still').show();
-  });
+$(document).ready(function(){
+    $('.ryu').mouseenter(function(){
+        $('.ryu-still').hide();
+        $('.ryu-ready').show();
+    })
+    .mouseleave(function() {
+        $('.ryu-still').show();
+        $('.ryu-ready').hide();
+    })
+    .mousedown(function(){
+        playHadouken();
+        $('.ryu-ready').hide();
+        $('.ryu-throwing').show();
+        $('.hadouken').finish().show().animate(
+            {'left': '1020px'},
+            500,
+            function(){
+                $(this).hide();
+                $(this).css('left', '520px');
+            });
+    })
+    .mouseup(function(){
+        $('.ryu-throwing').hide();
+        $('.ryu-ready').show();
+    });
+
 });
+
+function playHadouken() {
+    $('#hadouken-sound')[0].volume = 1;
+    $('#hadouken-sound')[0].load();
+    $('#hadouken-sound')[0].play();
+}
